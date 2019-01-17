e6bb58f21ec97146d5ec3700e39ae1ae0933d6e5
Hunter Metts <huntermettsdev@gmail.com>
2019-01-17 12:47:57 -0600

    setup_for_chap2

---
+++ src/components/location/LocationList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class EmployeeList  extends Component {
+    render() {
+        return (
+            <article>
+                <h1>Location List:</h1>
+                <h3>Nashville North</h3>
+                <section>1234 My Way</section>
+                <h3>Nashville South</h3>
+                <section>4321 Your Way</section>
+            </article>
+        );
+    }
+}
